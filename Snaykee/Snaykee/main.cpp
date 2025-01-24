6c162cecb604d3154fb2256652ba1085dc6f7696
McTiger Raphael <mctigerraphael25@gmail.com>
2025-01-23 19:33:53 -0500

    Initial Commit - Created and setup project for SFML.

---
+++ Snaykee/Snaykee/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	std::printf("Hello world [Snake Game]");
+
+	sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		while (const std::optional event = window.pollEvent())
+		{
+			if (event->is<sf::Event::Closed>())
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
